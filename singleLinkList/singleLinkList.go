1049f8a836b9f010dbfd830236777817c2ebf7c2
fenghui <fengh0409@qq.com>
2018-06-25 08:17:37 +0800

    update

--- singleLinkList/singleLinkList.go
+++ singleLinkList/singleLinkList.go
@@ -39,24 +39,47 @@ func (l *SingleLinkList) Append(e DataType) (DataType, error) {
 	l.Tail = node
 }
 
-func (l *SingleLinkList) Get(i int) (DataType, error) {
+func (l *SingleLinkList) Get(i int) (*Node, error) {
+	if err := checkIndex(i); err != nil {
+		return nil, err
+	}
+
+	node := l.Head
+	for j := 1; j < i; j++ {
+		node = node.Next
+	}
+
+	return node, nil
 
 }
 
 func (l *SingleLinkList) Insert(i int, e DataType) error {
-	node := &Node{
+	if err := checkIndex(i); err != nil {
+		return err
+	}
+
+	newNode := &Node{
 		Data: e,
 	}
 
-	if i < 1 || i > l.Length {
-		return fmt.Errorf("the index is invalid")
-	}
-
+	node := l.Head
 	for j := 1; j < i; j++ {
-
+		node = node.Next
 	}
+	newNode.Next = node.Next
+	node.Next = newNode
+
+	l.Length += 1
+
+	return nil
 }
 
 func (l *SingleLinkList) Delete(i int) (DataType, error) {
 
+}
+
+func (l *SingleLinkList) checkIndex(i int) error {
+	if i < 1 || i > l.Length {
+		return fmt.Errorf("the index is invalid")
+	}
 }
